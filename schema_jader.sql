8da23e872fd4830f7aae00d85cbeba47d30498e1
nar <nar@localhost.localdomain>
2014-06-06 12:46:14 +0900

    Change column names and data types

--- schema_jader.sql
+++ schema_jader.sql
@@ -2,53 +2,53 @@
 -- sql script to create tables and indexes
 
 CREATE TABLE demo (
-  case_id text not null unique,
-  freq integer not null,
-  sex text,
-  age text,
-  weight text,
-  height text,
-  quarter text,
-  scene text,
-  report text,
-  reporter text
+  case_id CHAR(10) NOT NULL unique,
+  freq SMALLINT NOT NULL,
+  sex VARCHAR(10),
+  age VARCHAR(20),
+  weight VARCHAR(20),
+  height VARCHAR(20),
+  quarter VARCHAR(10),
+  status VARCHAR(10),
+  report VARCHAR(10),
+  reporter VARCHAR(30)
 );
-CREATE INDEX demo01 on demo (case_id);
-CREATE INDEX demo02 on demo (quarter);
+CREATE INDEX demo01 ON demo (case_id);
+CREATE INDEX demo02 ON demo (quarter);
 CREATE TABLE drug (
-  case_id text not null,
-  freq integer not null,
-  sn integer,
-  relation text,
-  generic_name text,
-  brand_name text,
-  process text,
-  start_date text,
-  end_date text,
-  dosage text,
-  unit text,
-  fraction text,
-  reason text,
-  fix text,
-  relapse text
+  case_id CHAR(10) NOT NULL,
+  freq SMALLINT NOT NULL,
+  sn SMALLINT,
+  association VARCHAR(10),
+  name VARCHAR(120),
+  brand VARCHAR(50),
+  route VARCHAR(30),
+  start_date VARCHAR(20),
+  end_date VARCHAR(20),
+  dosage VARCHAR(20),
+  unit VARCHAR(10),
+  fraction VARCHAR(10),
+  reason VARCHAR(50),
+  fix VARCHAR(20),
+  relapse VARCHAR(10)
 );
-CREATE INDEX drug01 on drug (case_id);
-CREATE INDEX drug02 on drug (generic_name);
+CREATE INDEX drug01 ON drug (case_id);
+CREATE INDEX drug02 ON drug (name);
 CREATE TABLE reac (
-  case_id text not null,
-  freq integer not null,
-  sn integer,
-  event text,
-  outcome text,
-  date text
+  case_id CHAR(10) NOT NULL,
+  freq SMALLINT NOT NULL,
+  sn SMALLINT,
+  event VARCHAR(120),
+  outcome VARCHAR(10),
+  onset_date VARCHAR(20)
 );
-CREATE INDEX reac01 on reac (case_id);
-CREATE INDEX reac02 on reac (event);
+CREATE INDEX reac01 ON reac (case_id);
+CREATE INDEX reac02 ON reac (event);
 CREATE TABLE hist (
-  case_id text not null,
-  freq integer not null,
-  sn integer,
-  disease text
+  case_id CHAR(10) NOT NULL,
+  freq SMALLINT NOT NULL,
+  sn SMALLINT,
+  disease VARCHAR(120)
 );
-CREATE INDEX hist01 on hist (case_id);
-CREATE INDEX hist02 on hist (disease);
+CREATE INDEX hist01 ON hist (case_id);
+CREATE INDEX hist02 ON hist (disease);
